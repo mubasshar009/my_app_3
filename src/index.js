a0c3de0c2747ae89ad064e0c25b4ed70662817b1
Mubashar Hassan <mubasshar009@gmail.com>
2021-01-18 23:17:46 +0500

    Added

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+    //<App name = "Mubashar" age= "22" />
+    
+  ,
   document.getElementById('root')
 );
 
